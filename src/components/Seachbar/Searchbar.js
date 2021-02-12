800196da029f292dd643f6c388f12900118106c3
Robin Steinhauser <rbn2208@gmail.com>
2021-02-12 14:32:09 +0100

    Added searchbar

--- src/components/Seachbar/Searchbar.js
+++ src/components/Seachbar/Searchbar.js
@@ -1,10 +1,15 @@
 import createElement from '../../lib/createElement'
 import './Searchbar.css'
 
-export default function Searchbar() {
+export default function Searchbar(liveSearch) {
   const el = createElement('input', {
     className: 'Searchbar__bar',
     placeholder: 'Search',
+  })
+
+  el.addEventListener('input', event => {
+    const searchedValue = event.target.value
+    liveSearch(searchedValue)
   })
 
   return el
