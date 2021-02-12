01354c00fefa12f39ce1b41cd92f1e0d405a4031
Robin Steinhauser <rbn2208@gmail.com>
2021-02-12 23:04:46 +0100

    Added map, buttons will follow

--- src/components/MapPage/MapPage.js
+++ src/components/MapPage/MapPage.js
@@ -2,11 +2,17 @@ import createElement from '../../lib/createElement'
 import Homebutton from '../Homebutton'
 import './MapPage.css'
 
+import hogwartsMap from '../../assets/map.png'
+
 export default function MapPage(onNavigate) {
   const el = createElement('main', { className: 'Mappage', hidden: true })
   const homebutton = Homebutton(onNavigate)
-  el.append(homebutton)
-
+  const map = createElement('img', { src: hogwartsMap })
+  const mapContainer = createElement('div', {
+    className: 'Mappage__mapcontainer',
+  })
+  mapContainer.append(map)
+  el.append(homebutton, mapContainer)
   function show() {
     el.hidden = false
   }
