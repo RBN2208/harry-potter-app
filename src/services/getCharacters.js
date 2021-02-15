638a61e25d85bac9879f279591242fd24b87754f
Robin Steinhauser <rbn2208@gmail.com>
2021-02-15 13:42:02 +0100

    Fixed card import for vercel

--- src/services/getCharacters.js
+++ src/services/getCharacters.js
@@ -1,4 +1,4 @@
-const charactersUrl = 'http://hp-api.herokuapp.com/api/characters'
+const charactersUrl = 'https://hp-api.herokuapp.com/api/characters'
 
 export default function getCharacters() {
   return fetch(charactersUrl).then(res => res.json())
